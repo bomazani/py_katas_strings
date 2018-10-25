68a216a97e72c548d9f565b7475e794e7902653b
Bob Hunt <bob.hunt.email@gmail.com>
2018-10-25 10:18:29 -0400

    Completed all 4 katas.

--- main.py
+++ main.py
@@ -43,33 +43,31 @@ print("""2. Implement a function find_nth_occurrence
 that returns the index of the nth occurrence of a substring within a string 
 (considering that those substring could overlap each others). 
 If there are less than n occurrences of the substring, return -1.""")
-# find 'TestTest' in 'TestTestTestTest'. Find 1st,2nd,3rd,4th occurrences.
-# def find_nth_occurrence(number:)
-# sample = "TestTestTestTest"
-# term = "TestTest"
 
 
 def find_nth_occurrence(n, sub, string):
-    occurrences = 0
-    sub_len = len(sub)
-    for letter in string:
-        print(letter)
-        print("index: " + str(string.index(letter)))
-        start = string.index(letter)
-        end = start + sub_len
-        sample = string[start:end]
-        print("sample: " + sample)
-        if sample == sub:
-            occurrences += 1
-            print("occurrences: " + str(occurrences))
-            print("n = " + str(n))
-            print("index: " + str(start))
-            if occurrences == n:
-                answer = string.index(letter)
-    print(answer)
+    count = 0
+    start = 0
+
+    while count < n:
+        index = string.find(sub, start)
+        count += 1
+        start = index + 1
+
+    return index
 
 
-find_nth_occurrence(2, "TestTest", "TestTestTestTest")
+print("answer: " + str(find_nth_occurrence(1, "TestTest", "TestTestTestTest")))
+print("expect answer to be index: 0")
+print("answer: " + str(find_nth_occurrence(2, "TestTest", "TestTestTestTest")))
+print("expect answer to be index: 4")
+print("answer: " + str(find_nth_occurrence(3, "TestTest", "TestTestTestTest")))
+print("expect answer to be index: 8")
+print("answer: " + str(find_nth_occurrence(4, "TestTest", "TestTestTestTest")))
+print("expect answer to be index: -1")
+print("answer: " + str(find_nth_occurrence(5,
+                                           "TestTest", "TestTestTestTestTestTestTestTest")))
+print("expect answer to be index: 16")
 
 print('''3. Your beater car can\'t take another Indiana winter. 
 The shock absorbers are gone and you think it can handle about 15 more potholes 
@@ -91,8 +89,8 @@ def count_bumps(string):
 
 
 count_bumps(test1)
-
 count_bumps(test2)
+
 
 print("""4. Complete the function that accepts a string parameter, 
 and reverses each word in the string. 
@@ -110,5 +108,4 @@ def reverse_indiv_words(string):
 
 
 reverse_indiv_words("This is an example!")
-
 reverse_indiv_words("double  spaces")
